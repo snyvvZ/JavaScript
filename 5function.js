89274ca69c8fca0820e5df217b8d2d5947725563
김신영/프론트엔드개발팀 <ksy@musinsa.com>
2021-08-17 08:06:33 +0900

    array

--- 5function.js
+++ 5function.js
@@ -7,7 +7,7 @@ console.log(sum);
 // 함수로 만들기
 function add(c, d) {
   return c + d;
-  console.log('호출되지 않는 코드');
+  console.log("호출되지 않는 코드");
 }
 const sum2 = add(1, 2);
 console.log(sum2);
@@ -15,8 +15,8 @@ console.log(sum2);
 // 이름 넣기
 const hello = (name) => {
   console.log(`Hello, ${name}!`);
-}
-hello('snyvv');
+};
+hello("snyvv");
 
 // 함수 선언식(function)과 함수 표현식의 주요 차이점은
 // 화살표 함수에서 가르키는 this 와 function 에서 가르키는 this 가 서로 다르다는 것
